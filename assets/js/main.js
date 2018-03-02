d35011e2bd41e7e3cbbc9e0da7147f6eaeef3ce4
Phillip  Scott <phillipd20@yahoo.com>
2018-03-01 21:17:30 -0500

    pushing up code for merge

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,13 @@
-$(document).ready(function() {
-    $('#wrapper').fullpage({
-        sectionsColor: ['', '#d5d5d5', '#d5d5d5', '#d5d5d5', '#d5d5d5']
-    });
-});
+// $(document).ready(function() {
+//     $('#wrapper').fullpage({
+//         anchors: ['page1', 'page2', 'page3', 'page4'],
+//         sectionsColor: ['', '#d5d5d5', '#d5d5d5', '#d5d5d5', '#d5d5d5'],
+//         navigation: true,
+//         // normalScrollElements: '.womans-group-wrapper'
+//     });
+// });
+//
+//
+// $(document).on('click', '#moveTo', function() {
+//     $.fn.fullpage.moveTo('page2');
+// });
